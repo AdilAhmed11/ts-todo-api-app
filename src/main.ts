da206726220f1a207f66e40ec2456aeb4a7a62d4
Adil Ahmed <adil@Mac.cust.communityfibre.co.uk>
2025-01-29 12:48:45 +0000

    Greeting based on time works

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,30 @@
 import './style.css'
 
-const taskInput = document.getElementById<HTMLInputElement>("#taskInput");
-const addTaskBtn = document.getElementById<HTMLButtonElement>("#addTaskBtn");
-const clearTaskBtn = document.getElementById<HTMLButtonElement>("#clearTaskBtn");
-const greetingElement = document.getElementById<HTMLHeadingElement>("#greeting");
-const factElement = document.getElementById<HTMLParagraphElement>("#randomFact");
+const taskInput = document.getElementById<HTMLInputElement>("taskInput");
+const addTaskBtn = document.getElementById<HTMLButtonElement>("addTaskBtn");
+const clearTaskBtn = document.getElementById<HTMLButtonElement>("clearTaskBtn");
+const greetingElement = document.getElementById<HTMLHeadingElement>("greeting");
+const factElement = document.getElementById<HTMLParagraphElement>("randomFact");
 
-let setGreeting(): void {
-  const hour = new Date().getHours();
-  let greeting = "Good Evening!";
-  if (hour < 12) {
-    greeting = "Good Morning!";
-  } else if (hour < 18) {
-    greeting = "Good Afternoon!"
-  }
-  greetingElement?.innerText = greeting;
-}
 
-// const greetings = [
-//   "Bit early, isn't it?",
-//   "Good Morning",
-//   "Good Afternoon",
-//   "Good Evening"
-// ];
+// if (!taskInput || !addTaskBtn || !clearTaskBtn || !greetingElement || !factElement) {
+//     throw new Error('Some elements can not be found');
+// };
 
-// const index = Math.floor(new Date().getHours() / 24 * greetings.length);
-// document.getElementById("#greeting").innerHTML = greetings[index];
+const setGreeting = (): void => {
+    const hour = new Date().getHours();
+    const greeting = hour < 12 ? "Good Morning!" : hour < 18 ? "Good Afternoon!" : "Good Evening!";
+    greetingElement.textContent = greeting;
+};
+
+
+
+
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  setGreeting();
+    setGreeting();
 });
